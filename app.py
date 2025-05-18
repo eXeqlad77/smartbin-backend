858d9a508da33fcec80148e67180bb6d017214e4
eXeqIad77 <suleyman17551@gmail.com>
2025-05-18 19:36:27 +0300

    Render uyumlu port ayarı eklendi

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ def get_data():
     return jsonify({"status": "success"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host='0.0.0.0', port=port)
