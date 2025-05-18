babd3ddb6c2c8724f509da364bf42051378fc997
eXeqIad77 <suleyman17551@gmail.com>
2025-05-18 20:21:52 +0300

    Logs için print ile veri çıktısı eklendi

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 @app.route('/data', methods=['POST'])
 def get_data():
     data = request.get_json()
-    print("📦 Veri geldi:", data)
+    print("Veri geldi:", data)  # BU SATIR OLMAZSA LOGDA GÖRÜNMEZ!
     return jsonify({"status": "success"}), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000)) 
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
